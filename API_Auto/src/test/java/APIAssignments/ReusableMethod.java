c8af02511bd1519b85c7608e84361701004f2035
waghmaresuyog <suyogw1994@gmail.com>
2023-05-23 10:19:17 +0530

    formated

--- API_Auto/src/test/java/APIAssignments/ReusableMethod.java
+++ API_Auto/src/test/java/APIAssignments/ReusableMethod.java
@@ -2,14 +2,14 @@ package APIAssignments;
 
 public class ReusableMethod {
 
-        public static String getUrl()
-        {
-            String url="https://automationexercise.com/api/productsList";
-            return url;
-        }
-    int productId=1;
-    String productName="Blue Top";
-    String price="Rs. 500";
-    String brand="Polo";
-
+    public static String getUrl() {
+        String url = "https://automationexercise.com/api/productsList";
+        return url;
     }
+
+    int productId = 1;
+    String productName = "Blue Top";
+    String price = "Rs. 500";
+    String brand = "Polo";
+
+}
