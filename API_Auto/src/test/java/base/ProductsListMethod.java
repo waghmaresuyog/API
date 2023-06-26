dadf7a578b05e134791eaffbe567459a53dc0465
waghmaresuyog <suyogw1994@gmail.com>
2023-06-26 12:25:50 +0530

    service url is added

--- API_Auto/src/test/java/base/ProductsListMethod.java
+++ API_Auto/src/test/java/base/ProductsListMethod.java
@@ -7,10 +7,18 @@ import io.restassured.response.Response;
 
 import java.io.IOException;
 
-import static base.Base_URL.giveUrl;
+import static base.ServiceUrl.productBaseUrl;
 
 
 public class PostProductMethod extends EndPoint {
+
+    public static String finalUrl;
+
+    // combine base url and end point store into finalUrl and return it
+    public static String giveUrl() throws IOException {
+        finalUrl = productBaseUrl() + productEndURL();
+        return finalUrl;
+    }
 
     // Post request hit and return the response
     public Response postResponse() throws IOException {
--- API_Auto/src/test/java/base/ServiceUrl.java
+++ API_Auto/src/test/java/base/ServiceUrl.java
@@ -5,17 +5,19 @@ import java.io.IOException;
 import static endPoint.EndPoint.productEndURL;
 import static helper.UrlReader.getProperties;
 
-public class Base_URL {
-    public static String finalUrl;
-
+public class ServiceUrl {
     //This is Base url read from properties file return the base url
     public static String productBaseUrl() throws IOException {
         return getProperties().getProperty("UrlBase");
     }
+
+    public static String finalUrl;
 
     // combine base url and end point store into finalUrl and return it
     public static String giveUrl() throws IOException {
         finalUrl = productBaseUrl() + productEndURL();
         return finalUrl;
     }
+
+
 }
