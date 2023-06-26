6a792c87e30052bf03335f94217ba266bed221fb
waghmaresuyog <suyogw1994@gmail.com>
2023-06-26 14:59:55 +0530

    remove unwanted line

--- API_Auto/src/test/java/endPoint/EndPoint.java
+++ API_Auto/src/test/java/endPoint/EndPoint.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 public class EndPoint extends UrlReader {
     // return the end url
     public static String endUrl;
-    public static String getEndUrl;
     public static String productEndURL() {
         endUrl = "productsList";
         return endUrl;
