7bee69cc95df4d62a6a5348c308732750575a79d
waghmaresuyog <suyogw1994@gmail.com>
2023-06-27 13:00:09 +0530

    change in variable

--- API_Auto/src/test/java/endPoint/ServiceUrl.java
+++ API_Auto/src/test/java/endPoint/ServiceUrl.java
@@ -8,28 +8,25 @@ import static configReader.UrlReader.getProperties;
 public class ServiceUrl {
     //This is Base url read from properties file return the base url
 
-    public static String productBaseUrl()  {
+    public static String productBaseUrl() {
         String commonUrl;
         try {
-            commonUrl= getProperties().getProperty("UrlBase");
+            commonUrl = getProperties().getProperty("UrlBase");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return commonUrl;
     }
 
-    public static String getFinalUrl;
-
     public static String getUrl() {
-        getFinalUrl = productBaseUrl() + productEndURL();
+
+        String getFinalUrl = productBaseUrl() + productEndURL();
         return getFinalUrl;
     }
 
-    public static String finalUrl;
-
     // combine base url and end point store into finalUrl and return it
     public static String giveUrl() {
-        finalUrl = productBaseUrl() + productEndURL();
+        String finalUrl = productBaseUrl() + productEndURL();
         return finalUrl;
     }
 }
