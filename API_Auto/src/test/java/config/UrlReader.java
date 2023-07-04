2cb6d4108ee68a54b819df602a77b96f5893cb28
waghmaresuyog <suyogw1994@gmail.com>
2023-07-04 17:56:09 +0530

    update in path

--- API_Auto/src/test/java/config/UrlReader.java
+++ API_Auto/src/test/java/config/UrlReader.java
@@ -10,7 +10,7 @@ public class UrlReader {
     public static Properties getProperties() {
         Properties properties = new Properties();
         try {
-            FileInputStream file = new FileInputStream("src/test/java/configReader/Config.properties");
+            FileInputStream file = new FileInputStream("src/test/java/config/Config.properties");
             properties.load(file);
         } catch (IOException e) {
             throw new RuntimeException(e);
